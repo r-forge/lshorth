eabd12b3b41c821ec48a5e62c4ef8fc9a7a78cf8
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2007-09-03 11:39:32 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lshorth@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index_temp.php
+++ www/index_temp.php
@@ -21,13 +21,39 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
   </head>
 
-<body>
+<body leftmargin="0" topmargin="0" link="#0076d5" marginheight="0" marginwidth="0" text="#000000" vlink="#6a76d5">
+    <!--Top fragment-->
 
+    <table border="0" cellpadding="0" cellspacing="0" width="100%"><!-- table 1 -->
+      <tbody><tr>
+      
+      
+        <td width="40">&nbsp;</td>
 <! --- R-Forge Logo --- >
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
+<a href="http://r-forge.r-project.org/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td>
+
+        <td nowrap="nowrap">
+          <div align="center">
+            
+		<form action="/search/" method="get">
+		<table border="0" cellpadding="0" cellspacing="0">
+		<tbody><tr><td>
+		<div style="font-size: smaller;" align="center"><select name="type_of_search"><option value="full">Search the entire project</option>
+<option value="trackers">This project's trackers</option>
+<option value="forums">This project's forums</option>
+<option value="news">This project's news</option>
+<option value="soft">Software/Group</option>
+<option value="people">People</option>
+</select></div></td><td>&nbsp;<input value="54" name="group_id" type="hidden"></td><td><input size="12" name="words" value="" type="text"></td><td>&nbsp;</td><td><input name="Search" value="Search" type="submit"></td>
+					<td width="10">&nbsp;</td>
+					<td><b><a href="http://r-forge.r-project.org/search/advanced_search.php?group_id=54"> Advanced search</a></b></td></tr></tbody></table></form>          </div>
+        </td>
+
+
+ </tr>
+</tbody>
+</table><!-- table 1 -->
 
 
 <!-- get project title  -->
@@ -40,10 +66,18 @@ while (!feof($handle)) {
 }
 fclose($handle);
 echo $contents; } ?>
+<p>
+Technical notes with statistical background information will be installed as doc/TheShorthPlot.pdf or can be accessed 
+<a href="http://lshorth.r-forge.r-project.org/TheShorthPlot.pdf">from rforge</a>.
+</p>
 
 <!-- end of project description -->
-
-<strong>To install this package directly within R type <code>install.packages("<i>lshorth</i>",repos="http://r-forge.r-project.org")</code></strong>		<td nowrap="nowrap" valign="top" width="20%">
+<p>
+<strong>To install this package directly within R type </strong><br>
+&nbsp;&nbsp;&nbsp;<code>install.packages("<i>lshorth</i>",repos="http://r-forge.r-project.org")</code></p>
+<p><strong>To install the recent package from source directly within R type </strong><br>
+&nbsp;&nbsp;&nbsp;<code>install.packages("<i>lshorth</i>",repos="http://r-forge.r-project.org",type="source")</code></p>
+		<td nowrap="nowrap" valign="top" width="20%">
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
 </body>
