c95f347eef8c0cf99924f67c69c8c7cf7c72c85d
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2007-09-03 12:08:46 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lshorth@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/priv/index.php
+++ www/priv/index.php
@@ -16,7 +16,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
 
   <head>
-  	<meta http-equiv="refresh" content="5; URL=http://lshorth.r-forge.r-project.org/">
+  	<meta http-equiv="refresh" content="0; URL=http://lshorth.r-forge.r-project.org/">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
 	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
