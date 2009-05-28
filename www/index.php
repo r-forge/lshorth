3de34add34b6b58e61d976c596ba0ebbf7b5a895
gsawitzki (Günther Sawitzki) <gs@statlab.uni-heidelberg.de>
2009-05-28 12:16:01 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lshorth@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -198,6 +198,6 @@ See also:
 </p>
 <p>The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 </div>
-
+php
 </body>
 </html>
